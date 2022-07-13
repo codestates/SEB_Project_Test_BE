910326ffc42cfb6e6aba48d750614ff9ec81f42a
Hojin Jang <onewithtruth@gmail.com>
2022-07-13 15:08:00 +0800

    initial commit

--- src/main/java/com/codestates/srdemoref/order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codestates.srdemoref.order.repository;
-
-import com.codestates.srdemoref.order.entity.OrderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
-}
--- src/main/java/com/codestates/srdemoref/review/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codestates.srdemoref.review.repository;
-
-import com.codestates.srdemoref.review.entity.ReviewEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
-}
